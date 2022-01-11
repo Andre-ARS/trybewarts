e6e828e608cacf339cee3d0611300e75ae2413ef
André Rodrigues <andre360.ars@gmail.com>
2022-01-11 17:11:12 -0300

    Requisito 4 - adiciona titulo no header

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ let loginButton = document.querySelector('#login-button');
 
 // Requisito 3
 function loginCheck (event) {
-    event.preventDefault()
-
+    
     if (userEmail.value === "tryber@teste.com" && userPassword.value === "123456") {
         alert("Olá, Tryber!");
     } else {
